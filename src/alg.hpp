3ad67333b0d7a2da5dbc4104a90afb8cb5be3f01
rain <effcth@gmail.com>
2023-05-29 22:18:28 +0800

    2023529

--- src/alg.hpp
+++ src/alg.hpp
@@ -427,3 +427,48 @@ public:
     }
     
 };
+/**
+ * Definition for a binary tree node.
+ struct TreeNode {
+      int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution_create_tree_with_inorder_and_postorder_array {
+public:
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+    int index;
+    std::map<int,int> map;
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder){
+        index = postorder.size()-1;
+        for(int i=0;i<inorder.size();++i){
+            map[inorder[i]]=i;
+        }
+        return __builder(inorder,postorder,0,postorder.size()-1);
+    }
+    TreeNode* 
+    __builder(vector<int>& inorder,vector<int>& postorder,int left,int right){
+        if(left > right){
+            return nullptr;
+        }
+
+        int mid=map[postorder[index]];
+        TreeNode* cur = new TreeNode(postorder[index--]);
+
+        cur->right = __builder(inorder,postorder,mid+1,right);
+        cur->left = __builder(inorder,postorder,left,mid-1);
+        return cur;
+    }
+};
